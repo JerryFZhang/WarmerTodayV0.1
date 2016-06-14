feb1c6d015ddd0e72b8a856ef2242e5742ea21ea
zhang96 <jerry.fengwei@gmail.com>
2016-06-13 22:07:01 -0400

    Set the view engine, set the welcome page

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('port', process.env.PORT || 3000);
 
-//custom 404 page.
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');//Set the view engine to ejs for renderring html content.
+
+// Default landing page
+app.get('/', function(req, res) {
+    res.send('Welcome');
+});
+
+
+// Custom 404 page.
 app.use(function (req, res){
     res.type('text/plain');
     res.status(404);
-    res,send('404 - Not Found'); 
+    res.send('404 - Not Found'); 
 });
 
-//custom 500 page.
+// Custom 500 page.
 app.use(function (err, req, res, next){
     console.error(err.stack);
     res.type('text/plain');
@@ -18,8 +29,8 @@ app.use(function (err, req, res, next){
     res.send('500 - Server Error');    
 });
 
+// Start the server
 app.listen(app.get('port'), function(){
-    console.log('Express started on http://localhost:' + 
-               app.get('port' + '; press Ctrl-C to terminate.'));
+    console.log('Express started.');
 });
     
