e5898007941f8850fb28a71ab31c898e58d11b87
zhang96 <jerry.fengwei@gmail.com>
2016-06-13 21:43:40 -0400

    Clear up some spaces

--- app.js
+++ app.js
@@ -5,21 +5,17 @@ app.set('port', procecss.env.PORT || 3000);
 
 //custom 404 page.
 qpp.use(function (req, res){
-    
     res.type('text/plain');
     res.status(404);
-    res,send('404 - Not Found');
-    
+    res,send('404 - Not Found'); 
 });
 
 //custom 500 page.
 app.use(function (err, req, res, next){
-    
     console.error(err.stack);
     res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
-    
+    res.send('500 - Server Error');    
 });
 
 app.listen(app.get('port'), function(){
