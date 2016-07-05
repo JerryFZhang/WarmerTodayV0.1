867ab229ac7cd31496e3a52de1841c2ee11331fd
zhang96 <jerry.fengwei@gmail.com>
2016-07-04 21:37:25 -0400

    Deleted migrated files, now it works! Data passed to backend.

--- index.public.js
+++ index.public.js
@@ -1,61 +1,80 @@
-var http = require('http'),
-    port = 3000,
-    fs = require('fs');
-
 var express = require('express');
 
 //https://github.com/soplakanets/node-forecastio
-var forecastIO = require('forecastio');
-var forecastio = new forecastIO('******API Goes here*****');
+var forecaseIO = require('forecastio');
+var forecaseio = new forecaseIO('**********API KEY*************');
 
+var app = express();
 var path = require('path');
+var currentLocation;
+var bodyParser = require('body-parser');
 
-http.createServer(function (req, res) {
-    var path = req.url.replace(/\?(?:\?.*)?$/, '').toLowerCase();
-    switch (path) {
-    case '/':
-        fs.readFile('./public/index.html', function (err, data) {
-            res.writeHead(
-                200, {
-                    'Content-Type': 'text/html'
-                    , 'Content-Length': data.length
-                }
-            );
-            res.write(data);
-            res.end();
-        });
-        break;
 
-    case '/about':
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        res.end('About');
-        break;
+// Set the default port to localhost 3000.
+app.set('port', process.env.PORT || 3000);
 
-    case 'position':
-        break;
-            
-    case '/css/index.css':
-        fs.readFile('./public/css/index.css', function (err, data) {
-            res.writeHead(
-                200, {
-                    'Content-Type': 'text/css'
-                    , 'Content-Length': data.length
-                }
-            );
-            res.write(data);
-            res.end();
-        });
-        break;
-    default:
-        console.error('Path "' + path + '" not found');
-        res.writeHead(404, {
-            'Content-Type': 'text/plain'
-        });
-        res.end('404 Not Found');
-        break;
-    }
+//// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); //Set the view engine to ejs for renderring html content.
 
-}).listen(port);
-console.log('Server stared on localhost:' + port);
+app.use(bodyParser()); 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Default landing page
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+// Serving weather data at the moment using current location.
+app.get('/current', function (req, res) {
+    forecastio.forecast('51.506', '-0.127').then(function (data) {
+        var weatherInfo = data
+            , weatherInfoStringify = JSON.stringify(data)
+            , requestedLatitude = data.latitude
+            , requestedLongitude = data.longitude
+            , currentSummary = data.currently.summary;
+        console.log(weatherInfo);
+        console.log(weatherInfoStringify);
+        console.log(currentSummary);
+        res.send(JSON.stringify(data));
+    });
+
+});
+
+// serving historical data
+app.get('/old', function (req, res) {
+    forecastio.timeMachine('51.506', '-0.127', '2008-01-01T00:00:01Z').then(function (data) {
+        res.send(JSON.stringify(data, null, 2));
+    });
+
+})
+
+app.post('/position', function(req, res){
+    console.log(req.body);
+    console.log(req.query);
+    console.log(req.params);
+});
+
+
+//Serving static content directly from public folder, will consider do the same thing for html later as well.
+app.use('/static', express.static('public'));
+
+// Custom 404 page.
+app.use(function (req, res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+// Custom 500 page.
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+// Start the server
+app.listen(app.get('port'), function () {
+    console.log('Express started.');
+});
