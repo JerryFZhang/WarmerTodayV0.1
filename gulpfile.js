64fcb78299f797eb17bb3c6682ce0a84edc8557d
zhang96 <jerry.fengwei@gmail.com>
2016-07-12 21:30:58 -0400

    Moved gulp to dev dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp');
+var minify = require('gulp-minify');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+ 
+gulp.task('compress', function() {
+  gulp.src('lib/*.js')
+    .pipe(minify({
+        ext:{
+            src:'-debug.js',
+            min:'.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest('dist'))
 });
