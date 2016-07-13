25e220fa6076a983b5ea5aff06a73775593c12fc
zhang96 <jerry.fengwei@gmail.com>
2016-07-12 21:48:33 -0400

    Automated build work flow.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,14 @@
 var gulp = require('gulp');
 var minify = require('gulp-minify');
-
-gulp.task('default', function() {
-  // place code for your default task here
-});
-
- 
-gulp.task('compress', function() {
-  gulp.src('lib/*.js')
-    .pipe(minify({
-        ext:{
-            src:'-debug.js',
-            min:'.js'
-        },
-        exclude: ['tasks'],
-        ignoreFiles: ['.combo.js', '-min.js']
-    }))
-    .pipe(gulp.dest('dist'))
+gulp.task('compress', function () {
+    gulp.src('public/js/*.js')
+        //must delete all min files at this point
+        .pipe(minify({
+            ext: {
+                src: '.js'
+                , min: '.min.js'
+            }
+            , exclude: ['tasks']
+            , ignoreFiles: ['dev.js', 'min.js']
+        })).pipe(gulp.dest('public/js'))
 });
