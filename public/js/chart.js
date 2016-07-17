25be267a29c587618858bd434d7fcfd2a39f7e43
zhang96 <jerry.fengwei@gmail.com>
2016-07-16 22:50:20 -0400

    Changed x-axis labels. Populated the chart using dummy data.

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,79 +1,59 @@
 //Call handler
 var currentObject, currentSummary, currentIcon, currentTemp, hourlyObject, hourlySummary, hourlyIcon, hourlyData, dailyObject, dailySummary, dailyIcon, dailyIcon;
-    $('#current').click(function () {
-    
-        $.get('/current', {}, function (data) {
-            
-            data = JSON.parse(data);
-            
-            currentSummary = data.currently.summary;
-            currentIcon = data.currently.icon;
-            currentTemp = data.currently.temperature;
-            
-            hourlySummary = data.hourly.summary;
-            hourlyIcon = data.hourly.icon;
-            hourlyData = data.hourly.data;
-            
-            dailySummary = data.daily.summary;
-            dailyIcon = data.daily.icon;
-            dailyData = data.daily.data;
-            
-            $("p.inner").replaceWith( 
-                '<pre>' + currentSummary + '</pre><br>' + 
-                '<pre>' + currentIcon + '</pre><br>' + 
-                '<pre>' + currentTemp + '</pre><br>' + 
-                '<pre>' + hourlySummary + '</pre><br>' + 
-                '<pre>' + hourlyIcon + '</pre><br>' + 
-//                '<pre>' + hourlyData[1].time + '</pre><br>' +
-                '<pre>' + dailySummary + '</pre><br>' + 
-                '<pre>' + dailyIcon + '</pre><br>' + 
-                '<pre>' + JSON.stringify(dailyData) +'</pre><br>');
-            
-            for (var i = 0; i < hourlyData.length; i++ ){
-                var time = hourlyData[i].time;
-                var date = new Date(time*1000);
-                var f = parseFloat(hourlyData[i].apparentTemperature);
-                var c = (f - 32) * 5 / 9;
-                console.log(hourlyData[i].summary);
-                console.log(date);
-                console.log(c.toFixed(2) + 'ºC');
-                console.log(hourlyData[i].windSpeed);
-            };
-            
-        });
+$('#current').click(function () {
+    $.get('/current', {}, function (data) {
+        data = JSON.parse(data);
+        currentSummary = data.currently.summary;
+        currentIcon = data.currently.icon;
+        currentTemp = data.currently.temperature;
+        hourlySummary = data.hourly.summary;
+        hourlyIcon = data.hourly.icon;
+        hourlyData = data.hourly.data;
+        dailySummary = data.daily.summary;
+        dailyIcon = data.daily.icon;
+        dailyData = data.daily.data;
+        $("p.inner").replaceWith('<pre>' + currentSummary + '</pre><br>' + '<pre>' + currentIcon + '</pre><br>' + '<pre>' + currentTemp + '</pre><br>' + '<pre>' + hourlySummary + '</pre><br>' + '<pre>' + hourlyIcon + '</pre><br>' +
+            //                '<pre>' + hourlyData[1].time + '</pre><br>' +
+            '<pre>' + dailySummary + '</pre><br>' + '<pre>' + dailyIcon + '</pre><br>' + '<pre>' + JSON.stringify(dailyData) + '</pre><br>');
+        for (var i = 0; i < hourlyData.length; i++) {
+            var time = hourlyData[i].time;
+            var date = new Date(time * 1000);
+            var f = parseFloat(hourlyData[i].apparentTemperature);
+            var c = (f - 32) * 5 / 9;
+            console.log(hourlyData[i].summary);
+            console.log(date);
+            console.log(c.toFixed(2) + 'ºC');
+            console.log(hourlyData[i].windSpeed);
+        };
     });
-    
-    $('#old').click(function () {
-        
-        $.get('/old', {}, function (data) {
-        
-            $("p.inner").replaceWith()
-        
-        });
+});
+$('#old').click(function () {
+    $.get('/old', {}, function (data) {
+        $("p.inner").replaceWith()
     });
-
+});
 // Chart populator
 var ctx = document.getElementById("c").getContext("2d");
-    var data = {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [{
-        label: "My First dataset",
-        fillColor: "rgba(220,220,220,0.2)",
-        strokeColor: "rgba(220,220,220,1)",
-        pointColor: "rgba(220,220,220,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)",
-        data: [65, 59, 80, 81, 56, 55, 40]
+var data = {
+    labels: ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm", "12pm", "1am", "2am", "3am", "4am", "5am", "6am", "7am"]
+    , datasets: [{
+        label: "My First dataset"
+        , fillColor: "rgba(220,220,220,0.2)"
+        , strokeColor: "rgba(220,220,220,1)"
+        , pointColor: "rgba(220,220,220,1)"
+        , pointStrokeColor: "#fff"
+        , pointHighlightFill: "#fff"
+        , pointHighlightStroke: "rgba(220,220,220,1)"
+        , data: [12,15,15,17,19,23,23,24,24,24,25,26,27,19,17,16,15,15,15,14,13,13,15,16]
       }, {
-        label: "My Second dataset",
-        fillColor: "rgba(151,187,205,0.2)",
-        strokeColor: "rgba(151,187,205,1)",
-        pointColor: "rgba(151,187,205,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(151,187,205,1)",
-        data: [28, 48, 40, 19, 86, 27, 90]
+        label: "My Second dataset"
+        , fillColor: "rgba(151,187,205,0.2)"
+        , strokeColor: "rgba(151,187,205,1)"
+        , pointColor: "rgba(151,187,205,1)"
+        , pointStrokeColor: "#fff"
+        , pointHighlightFill: "#fff"
+        , pointHighlightStroke: "rgba(151,187,205,1)"
+        , data: [23,23,24,24,24,25,26,27,30,30,30,31,33,35,24,24,23,22,21,19,17,16,14,15]
       }]
-    };
-    var MyNewChart = new Chart(ctx).Line(data);
+};
+var MyNewChart = new Chart(ctx).Line(data);
