29421084e15db0ff24f344d56fa45898ee90f980
zhang96 <jerry.fengwei@gmail.com>
2016-07-27 18:48:54 -0400

    code cleanup

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,6 +1,6 @@
-var lat,lng;
+var lat, lng, hourlyData;
 var positionJSON = {lat: lat, lng: lng};
-var currentObject, currentSummary, currentIcon, currentTemp, hourlyObject, hourlySummary, hourlyIcon, hourlyData, dailyObject, dailySummary, dailyIcon, dailyIcon;
+
 var hourlyDataToCel = [];
 
 if (navigator.geolocation) {
@@ -13,7 +13,6 @@ else {
 }
 
 function showPosition(position) {
-    
     // Store latitude and longitude as a float from the json object.
     lat = parseFloat(JSON.stringify(position.coords.latitude));
     lng = parseFloat(JSON.stringify(position.coords.longitude));
@@ -23,12 +22,11 @@ function showPosition(position) {
     
     // Post the location data to backend and get the weather information.
     $.post('/current', {lat: lat, lng: lng}, function (data) {
-    
         data = JSON.parse(data);
         hourlyData = data.hourly.data;
         
         $("p.inner").replaceWith('<h2>' + data.currently.summary + '</h2><br>' + '<h2>' + convertToCelcius(data.currently.temperature) + ' Cº</h2><br>');
-        
+       
         for (var i = 0; i < hourlyData.length; i++) {
             var time = hourlyData[i].time;
             var date = new Date(time * 1000);
@@ -38,13 +36,11 @@ function showPosition(position) {
         
         //chart
         var ctx = document.getElementById("c").getContext("2d");
-        
         // Calculating time lables
         var time = new Date;
         var hour = time.getHours();
         var lables = []
         var lablesString = []
-        
         for (var i = 0; i < 24; i++) {
             var addedHour = i + hour;
             if (addedHour > 24) {
@@ -54,7 +50,6 @@ function showPosition(position) {
                 lables[i] = hour + i;
             }
         }
-        
         for (var i = 0; i < 24; i++) {
             switch (true) {
             case lables[i] > 0 && lables[i] < 12:
@@ -71,7 +66,6 @@ function showPosition(position) {
                 break;
             }
         }
-        
         var data = {
             labels: lablesString
             , datasets: [{
@@ -85,21 +79,24 @@ function showPosition(position) {
                     , data: hourlyDataToCel
       }
                       , {
-             label: "My Second dataset"
-             , fillColor: "rgba(151,187,205,0.2)"
-             , strokeColor: "rgba(151,187,205,1)"
-             , pointColor: "rgba(151,187,205,1)"
-             , pointStrokeColor: "#fff"
-             , pointHighlightFill: "#fff"
-             , pointHighlightStroke: "rgba(151,187,205,1)"
-             , data: [23, 23, 24, 24, 24, 25, 26, 27, 30, 30, 30, 31, 33, 35, 24, 24, 23, 22, 21, 19, 17, 16, 14, 15]
+                    label: "My Second dataset"
+                    , fillColor: "rgba(151,187,205,0.2)"
+                    , strokeColor: "rgba(151,187,205,1)"
+                    , pointColor: "rgba(151,187,205,1)"
+                    , pointStrokeColor: "#fff"
+                    , pointHighlightFill: "#fff"
+                    , pointHighlightStroke: "rgba(151,187,205,1)"
+                    , data: [23, 23, 24, 24, 24, 25, 26, 27, 30, 30, 30, 31, 33, 35, 24, 24, 23, 22, 21, 19, 17, 16, 14, 15]
       }
                      ]
         };
         var MyNewChart = new Chart(ctx).Line(data);
     });
-    $.post('/old'    , {lat: lat, lng: lng}, function (data) {
-        $("p.inner2").replaceWith('<pre>'+data+'</pre>')
+    $.post('/old', {
+        lat: lat
+        , lng: lng
+    }, function (data) {
+        $("p.inner2").replaceWith('<pre>' + data + '</pre>')
     });
 }
 
@@ -107,5 +104,3 @@ function convertToCelcius(fren) {
     var celc = (fren - 32) * 5 / 9;
     return celc.toFixed(0);
 }
-
-
