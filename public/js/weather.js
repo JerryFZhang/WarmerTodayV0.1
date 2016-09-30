d007151cf04ff3f404c9bc6154f0b3b37767eb81
zhang96 <jerry.fengwei@gmail.com>
2016-09-30 17:10:43 -0400

    ui tweaks

--- public/js/weather.js
+++ public/js/weather.js
@@ -17,7 +17,7 @@ function convertToCelcius(fren) {
 }
 
 function getTimeArray(){
-    return ['12pm','1am','2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm'];
+    return ['12pm','','', '3am', '', '', '6am', '', '', '9am', '', '', '12pm', '', '', '3pm', '', '', '6pm', '', '', '9pm', '', '11pm'];
 }
 
 function loadChart(currentHourlyDataToCel, oldHourlyDataToCel) {
